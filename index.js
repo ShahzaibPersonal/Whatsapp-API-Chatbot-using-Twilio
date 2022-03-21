2e645f45daabfd5f3e718c9c407b1963089d779b
ShahzaibPersonal <52831453+ShahzaibPersonal@users.noreply.github.com>
2022-03-21 10:44:29 +0500

    asda

--- index.js
+++
@@ -1,7 +0,0 @@
-// API key: 8bb6ef1b66972ca3c8b998e1a7e4aa32
-
-// api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-
-//  twillio 9Le-HgvLMlQDlM4v7aWY_0COaZYh6yVtzrQJ0Itw
-
-
